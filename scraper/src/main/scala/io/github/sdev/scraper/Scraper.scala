78ab992bba3819ccc51def9099250ec71fda08bb
Sergio Cano <canosergio90@gmail.com>
2022-03-28 17:29:11 -0300

    implementing library - needs fixes
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- scraper/src/main/scala/io/github/sdev/scraper/Scraper.scala
+++ scraper/src/main/scala/io/github/sdev/scraper/Scraper.scala
@@ -1,6 +1,27 @@
 package io.github.sdev.scraper
 
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+import net.ruippeixotog.scalascraper.model.Element
+
 import zio.IO
+import zio.Task
 
 object Scraper:
-  def scrapNews(siteUrl: String): IO[ScrapError, List[News]] = ???
+  // FIXME
+  def scrapNews(siteUrl: String): IO[ScrapError, List[News]] =
+    for
+      browser <- Task.effect(JsoupBrowser())
+      doc     <- Task.effect(browser.get(siteUrl))
+      stories <- Task.effect(doc >> elementList("#site-content section [class=story-wrapper]"))
+      news    <- stories.map(parseContent).collect { case Right(news) => news }
+    yield news
+
+  private def parseContent(element: Element): Either[ScrapError, News] =
+    val title     = element >> allText("h3")
+    val maybeLink = (element >> "a" >> attrs("href")).headOption
+    maybeLink match
+      case Some(link) => Right(News(title, link))
+      case None       => Left(ScrapError(element, "doesn't have link"))

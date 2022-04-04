1946089d16034c131d0bb441423e39ec953168df
Sergio Cano <canosergio90@gmail.com>
2022-04-04 18:35:24 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- core/src/main/scala/io/github/sdev/adapter/out/cache/CacheServiceImpl.scala
+++ core/src/main/scala/io/github/sdev/adapter/out/cache/CacheServiceImpl.scala
@@ -1,5 +1,10 @@
 package io.github.sdev.adapter.out.cache
 
 import io.github.sdev.application.ports.out.CacheService
+import cats.effect.IO
+import io.github.sdev.scraper.News
 
-class CacheServiceImpl extends CacheService {}
+class CacheServiceImpl extends CacheService {
+  // TODO
+  override def getAll(): IO[List[News]] = ???
+}

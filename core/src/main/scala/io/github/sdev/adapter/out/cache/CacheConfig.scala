7fc21c7eb00d0140da304ab48e0f338aeceaa838
Sergio Cano <canosergio90@gmail.com>
2022-04-05 21:16:41 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- core/src/main/scala/io/github/sdev/adapter/out/cache/CacheConfig.scala
+++ core/src/main/scala/io/github/sdev/adapter/out/cache/CacheConfig.scala
@@ -1,5 +1,13 @@
 package io.github.sdev.adapter.out.cache
 
 import scala.concurrent.duration.FiniteDuration
+import dev.profunktor.redis4cats.effects
 
-case class CacheConfig(ttl: FiniteDuration)
+import scala.concurrent.duration._
+
+case class CacheConfig(ttl: effects.SetArg.Ttl.Px)
+
+object CacheConfig {
+
+  def apply(ttl: Int): CacheConfig = new CacheConfig(effects.SetArg.Ttl.Px(ttl.millis))
+}

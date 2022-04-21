b289309257a2b93d2d41d6c4cd8db545b7ccd752
Sergio Cano <canosergio90@gmail.com>
2022-04-21 20:17:25 -0300

    domain and application layers refactor with tf and polimorphic effects
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- core/src/main/scala/io/github/sdev/application/GetNewsUseCaseService.scala
+++ core/src/main/scala/io/github/sdev/application/GetNewsUseCaseService.scala
@@ -1,25 +1,32 @@
 package io.github.sdev.application
 
 import io.github.sdev.domain.usecases.GetNewsUseCase
-import cats.effect.IO
 import io.github.sdev.scraper.News
 import io.github.sdev.application.ports.out.ScraperService
 import io.github.sdev.application.ports.out.CacheService
 import io.github.sdev.application.ports.out.NewsRepository
 
+import cats.syntax.all._
+import cats.Monad
+import cats.effect.IO
+import cats.Applicative
+
 // TODO add tagless final to this algebra (we are hardcoding IO here)
-class GetNewsUseCaseService(scraperService: ScraperService[IO], newsRepository: NewsRepository[IO], cache: CacheService[IO])
-    extends GetNewsUseCase {
-  override def getNews(): IO[List[News]] =
+class GetNewsUseCaseService[F[_]: Monad: Applicative](
+    scraperService: ScraperService[F],
+    newsRepository: NewsRepository[F],
+    cache: CacheService[F]
+) extends GetNewsUseCase[F] {
+  override def getNews(): F[List[News]] =
     for {
       cachedNews <- cache.getAll()
       isCacheEmpty = cachedNews.isEmpty
       news <-
         if (isCacheEmpty)
           newsRepository.findAll()
-        else IO.pure(cachedNews)
-      _ <- IO.whenA(isCacheEmpty)(cache.save(news))
+        else cachedNews.pure[F]
+      _ <- Applicative[F].whenA(isCacheEmpty)(cache.save(news))
       areNewHeadlinesAvailable = isCacheEmpty
-      _ <- IO.whenA(isCacheEmpty)(newsRepository.save(news))
+      _ <- Applicative[F].whenA(isCacheEmpty)(newsRepository.save(news))
     } yield news
 }

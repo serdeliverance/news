2070cc5a7562a9f9fb1898581dac13b27358f695
Sergio Cano <canosergio90@gmail.com>
2022-04-21 20:35:25 -0300

    adapter layer tagless final refactor (#7)
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- core/src/main/scala/io/github/sdev/Main.scala
+++ core/src/main/scala/io/github/sdev/Main.scala
@@ -3,8 +3,9 @@ package example.io.github.sdev
 import cats.effect.IOApp
 import io.github.sdev.adapter.in.web.HttpServer
 import cats.effect.ExitCode
+import cats.effect.IO
 
 object Main extends IOApp {
   def run(args: List[String]) =
-    HttpServer.make.compile.drain.as(ExitCode.Success)
+    HttpServer.make[IO].compile.drain.as(ExitCode.Success)
 }

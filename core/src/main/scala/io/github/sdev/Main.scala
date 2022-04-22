e5b7c298d6d6e215493fdf642e24194a67952bb7
Sergio Cano <canosergio90@gmail.com>
2022-04-22 12:15:49 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- core/src/main/scala/io/github/sdev/Main.scala
+++ core/src/main/scala/io/github/sdev/Main.scala
@@ -1,11 +1,55 @@
 package example.io.github.sdev
 
 import cats.effect.IOApp
-import io.github.sdev.adapter.in.web.HttpServer
 import cats.effect.ExitCode
-import cats.effect.IO
+
+import org.http4s.ember.client.EmberClientBuilder
+import fs2.Stream
+import org.http4s.ember.client.EmberClientBuilder
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.implicits._
+import org.http4s.server.middleware.Logger
+import cats.syntax.all._
+import com.comcast.ip4s._
+import cats.effect.Resource
+import cats.effect.Async
+
+import io.github.sdev.adapter.in.web.NewsRoutes
+import io.github.sdev.application.GetNewsUseCaseService
+import io.github.sdev.application.ports.out.ScraperService
+import io.github.sdev.scraper.ScraperServiceImpl
+import io.github.sdev.adapter.out.persistence.NewsRepositoryImpl
+import io.github.sdev.application.ports.out.CacheService
+import io.github.sdev.adapter.out.cache.CacheServiceImpl
+import io.github.sdev.adapter.out.cache.CacheConfig
 
 object Main extends IOApp {
-  def run(args: List[String]) =
-    HttpServer.make[IO].compile.drain.as(ExitCode.Success)
+
+  def createServer[F[_]: Async]: Stream[F, Nothing] = {
+    for {
+      client      <- Stream.resource(EmberClientBuilder.default[F].build)
+      sessions    <- ??? // TODO create skunk session
+      redisClient <- ??? // TODO create redis client
+      cacheConfig           = CacheConfig(1000 * 3600)  // TODO remove hardcoding and extract from configuration
+      scraperService        = new ScraperServiceImpl[F] // TODO add logger (it conflicts with line 35)
+      newsRepository        = new NewsRepositoryImpl[F](sessions)
+      cacheService          = new CacheServiceImpl[F](redisClient, cacheConfig)
+      getNewsUseCaseService = new GetNewsUseCaseService[F](scraperService, newsRepository, cacheService)
+      httpApp = (
+        NewsRoutes.endpoints[F](getNewsUseCaseService)
+      ).orNotFound
+      finalHttpApp = Logger.httpApp(true, true)(httpApp)
+      exitCode <- Stream.resource(
+        EmberServerBuilder
+          .default[F]
+          .withHost(ipv4"0.0.0.0")
+          .withPort(port"8080")
+          .withHttpApp(finalHttpApp)
+          .build >>
+          Resource.eval(Async[F].never)
+      )
+    } yield exitCode
+  }.drain
+
+  def run(args: List[String]) = ???
 }
--- core/src/main/scala/io/github/sdev/adapter/in/web/NewsRoutes.scala
+++ core/src/main/scala/io/github/sdev/adapter/in/web/NewsRoutes.scala
@@ -4,13 +4,16 @@ import cats.implicits._
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import cats.effect.Sync
+import io.github.sdev.application.GetNewsUseCaseService
+import org.http4s.circe.CirceEntityDecoder._
 
 object NewsRoutes {
-  def routes[F[_]: Sync]: HttpRoutes[F] = {
+
+  def endpoints[F[_]: Sync](getNewsUseCaseService: GetNewsUseCaseService[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] { case GET -> Root / "news" =>
-      Ok("Ok")
+      getNewsUseCaseService.getNews().flatMap(Ok(_))
     }
   }
 }

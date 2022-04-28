df986762e5c455dc8bfe31e7859c6f26d03042e0
Sergio Cano <canosergio90@gmail.com>
2022-04-28 19:44:57 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/test/scala/io/github/sdev/application/GetNewsUseCaseServiceSpec.scala
+++ src/test/scala/io/github/sdev/application/GetNewsUseCaseServiceSpec.scala
@@ -9,8 +9,9 @@ import io.github.sdev.application.ports.out.NewsRepository
 import io.github.sdev.application.ports.out.CacheService
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import io.github.sdev.domain.entities.News
+import io.github.sdev.stubs.NewsStubs
 
-class GetNewsUseCaseServiceSpec extends CatsEffectSuite {
+class GetNewsUseCaseServiceSpec extends CatsEffectSuite with NewsStubs {
 
   implicit val logger = Slf4jLogger.getLogger[IO]
 
@@ -24,13 +25,18 @@ class GetNewsUseCaseServiceSpec extends CatsEffectSuite {
   test("getting news from web site") {
     val result = subject.getNews()
 
-    // TODO finish implementation
+    when(cacheService.getAll()).thenReturn(IO.pure(List.empty))
+    when(scraperService.scrapNews(url)).thenReturn(IO.pure(aListOfNews))
+    when(newsRepository.save(aListOfNews)).thenReturn(IO.unit)
+    when(cacheService.save(aListOfNews)).thenReturn(IO.unit)
+
     assertIO(result, List(News("A new", "https://nytimes.com/a-new"), News("Another new", "https://nytimes.com/another-new")))
   }
 
   test("getting news from cache") {
     val result = subject.getNews()
 
+    when(cacheService.getAll()).thenReturn(IO.pure(aListOfNews))
     // TODO finish implementation
     assertIO(result, List(News("A new", "https://nytimes.com/a-new"), News("Another new", "https://nytimes.com/another-new")))
   }
@@ -40,7 +46,7 @@ class GetNewsUseCaseServiceSpec extends CatsEffectSuite {
 
     // TODO finish implementation
     result.attempt.map {
-      case Left(ex)     => assert(ex == ScrapingError)
+      case Left(ex)     => assert(ex == new IllegalAccessException)
       case Right(value) => fail("ble")
     }
   }
